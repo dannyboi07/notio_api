1be7ba81fc53acdccc61ebeb3434be5196ec01b5
dannyboi07 <danielchettiar07@gmail.com>
2023-07-29 10:21:38 +0530

    Updates schema for kanban column creation, gets

--- schema/kanban.js
+++ schema/kanban.js
@@ -57,9 +57,62 @@ const GetCreateKanbanResponse = (createdKanban) =>
 const GetManyKanbanBoardsResponse = (kanbanBoards) =>
     kanbanBoards.map((kanbanBoard) => GetKanbanBoardResponse(kanbanBoard));
 
+const CreateKanbanBoardColumn = Joi.object({
+    title: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+        .messages({
+            "any.required": getStringRequired("Title"),
+            "string.empty": getStringEmpty("Title"),
+            "string.min": getStringMinLen("Title"),
+            "string.max": getStringMaxLen("Title"),
+        }),
+    description: Joi.string()
+        .min(3)
+        .max(50)
+        .required()
+        .messages({
+            "any.required": getStringRequired("Description"),
+            "string.empty": getStringEmpty("Description"),
+            "string.min": getStringMinLen("Description"),
+            "string.max": getStringMaxLen("Description"),
+        }),
+    position: Joi.number()
+        .required()
+        .messages({
+            "any.required": getStringRequired("Position"),
+            "number.base": getStringRequired("Position"),
+        }),
+});
+
+/**
+ * @param {KanbanColumn} kanbanBoardColumn
+ * @returns {KanbanColumn}
+ */
+const GetKanbanBoardColumnResponse = (kanbanBoardColumn) => ({
+    id: kanbanBoardColumn.id,
+    board_id: kanbanBoardColumn.board_id,
+    title: kanbanBoardColumn.title,
+    description: kanbanBoardColumn.description,
+    position: kanbanBoardColumn.position,
+    created_at: kanbanBoardColumn.created_at,
+    updated_at: kanbanBoardColumn.updated_at,
+});
+
+/**
+ * @param {KanbanColumn} createdKanbanBoardColumn
+ * @returns {KanbanColumn}
+ */
+const GetCreateKanbanBoardColumnResponse = (createdKanbanBoardColumn) =>
+    GetKanbanBoardColumnResponse(createdKanbanBoardColumn);
+
 module.exports = {
     CreateKanbanBoard,
     GetCreateKanbanResponse,
     GetKanbanBoardResponse,
     GetManyKanbanBoardsResponse,
+    CreateKanbanBoardColumn,
+    GetKanbanBoardColumnResponse,
+    GetCreateKanbanBoardColumnResponse,
 };
