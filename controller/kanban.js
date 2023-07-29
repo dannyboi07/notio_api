516d10bb0a6e0c8fc2928afb1ee5e7f9ec9fc96a
dannyboi07 <danielchettiar07@gmail.com>
2023-07-29 10:24:31 +0530

    Adds get all boards, create column route

--- controller/kanban.js
+++ controller/kanban.js
@@ -7,7 +7,25 @@ const {
     CreateKanbanBoard,
     GetCreateKanbanResponse,
     GetManyKanbanBoardsResponse,
+    CreateKanbanBoardColumn,
+    GetCreateKanbanBoardColumnResponse,
 } = require("../schema/kanban");
+const { HTTP400Error } = require("../common/exceptions");
+
+router.get("/all", AuthMiddleware, async (req, res) => {
+    try {
+        const boards = await KanbanService.GetAllUserBoards(req.userDetails.id);
+        return res.json({
+            status: "success",
+            data: GetManyKanbanBoardsResponse(boards),
+        });
+    } catch (error) {
+        return res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+    }
+});
 
 router.post(
     "/",
@@ -33,20 +51,39 @@ router.post(
     },
 );
 
-router.get("/all", AuthMiddleware, async (req, res) => {
-    try {
-        const boards = await KanbanService.GetAllUserBoards(req.userDetails.id);
-        return res.json({
-            status: "success",
-            data: GetManyKanbanBoardsResponse(boards),
-        });
-    } catch (error) {
-        return res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message,
-        });
-    }
-});
+router.post(
+    "/:id/column",
+    AuthMiddleware,
+    InputValidation(CreateKanbanBoardColumn),
+    async (req, res) => {
+        try {
+            let boardId = null;
+            try {
+                boardId = parseInt(req.params.id, 10);
+            } catch (error) {
+                throw new HTTP400Error("Invalid board id");
+            }
+
+            const createdColumn = await KanbanService.CreateKanbanBoardColumn(
+                {
+                    ...req.body,
+                    board_id: req.params.id,
+                },
+                req.userDetails.id,
+            );
+            return res.json({
+                status: "success",
+                message: "Kanban board column created successfully",
+                data: GetCreateKanbanBoardColumnResponse(createdColumn),
+            });
+        } catch (error) {
+            return res.status(error.statusCode).json({
+                status: error.status,
+                message: error.message,
+            });
+        }
+    },
+);
 
 module.exports = {
     router,
